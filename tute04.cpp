e4f3782b45a614c06ec3500abe4e24b3ede8f2e9
IT21316272 <99665491+IT21316272@users.noreply.github.com>
2022-02-16 11:46:10 +0000

    Tutorial2 exercise4 completed

--- tute04.cpp
+++ tute04.cpp
@@ -11,7 +11,7 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
-#include <iostream>
+/*#include <iostream>
 
 long Factorial(int no);
 long nCr(int n, int r);
@@ -26,4 +26,39 @@ int main() {
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+} */
+
+#include <iostream>
+
+long Factorial(int no);
+long nCr(int n, int r);
+
+int main() {
+  int n, r;
+  std::cout << "Enter a value for n ";
+  std::cin >> n;
+  std::cout << "Enter a value for r ";
+  std::cin >> r;
+	
+std::cout << "nCr = ";
+ std::cout << nCr(n,r);
+ std::cout << std::endl;
+	return 0;
+}
+
+long nCr(int n, int r)//to find factorial of value r
+{
+	
+	return Factorial(n)/(Factorial(r)*Factorial(n-r));
+}
+
+long Factorial(int no)//to find factorial of value n
+{
+	long fac_n=1;
+	for(int i=1; i<=no; i++)
+	{
+		fac_n=fac_n*i;
+	}
+
+	return fac_n;
 }
